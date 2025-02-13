5f63c8d921c0ae8814e8a003ab448ecb898eb546
CristianUriel21 <cristianuriel.ramirezhernandez@unidemat.edu.mx>
2025-02-13 11:37:35 -0600

    Nuevos trabajos
    Se agregaron practicas del 1 al 3

---
+++ Practica 01.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_01
+{
+    public partial class Form1 : Form
+    {
+        private object  vResultado;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmulti_Click(object sender, EventArgs e)
+        {
+            vResultado = Convert.ToInt32(num1.Text) * Convert.ToInt32(num2.Text);
+            lblResult.Text = vResultado.ToString();
+            lblsigno.Text = "X";
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnresta_Click(object sender, EventArgs e)
+        {
+
+            vResultado = Convert.ToInt32(num1.Text) - Convert.ToInt32(num2.Text);
+            lblResult.Text = vResultado.ToString();
+            lblsigno.Text = "-";
+        }
+
+        private void btnsuma_Click(object sender, EventArgs e)
+        {
+
+            vResultado = Convert.ToInt32(num1.Text) + Convert.ToInt32(num2.Text);
+            lblResult.Text = vResultado.ToString();
+            lblsigno.Text = "+";
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            num1.Text = "";
+            num2.Text = "";
+            lblResult.Text = "";
+            lblsigno.Text = "";
+        }
+
+        private void btndivi_Click(object sender, EventArgs e)
+        {
+
+            vResultado = Convert.ToInt32(num1.Text) / Convert.ToInt32(num2.Text);
+            lblResult.Text = vResultado.ToString();
+            lblsigno.Text = "/";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void num1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblsigno_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Practica 03.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_03
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtradio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnperimetro_Click(object sender, EventArgs e)
+        {
+            Double ResultadoPe;
+            Double Pi = 3.1416;
+
+            ResultadoPe = 2 * Pi * Convert.ToDouble(txtradio.Text);
+            resultadop.Text= ResultadoPe.ToString();
+
+
+        }
+
+        private void btnarea_Click(object sender, EventArgs e)
+        {
+            Double ResultadoA;
+            Double pi = 3.1416;
+
+            ResultadoA = Math.Pow(Convert.ToDouble(txtradio.Text), 2) * pi;
+            resultador.Text= ResultadoA.ToString();
+        }
+
+        private void resultador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultadop_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
