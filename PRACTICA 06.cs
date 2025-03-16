c30e002d6182e7dae83beba6039c24aceb6a6225
CristianUriel21 <cristianuriel.ramirezhernandez@unidemat.edu.mx>
2025-03-15 21:56:54 -0500

    Add files via upload
    practica de login y vinculada con base de datos

---
+++ PRACTICA 06.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOGIN_Y_BASE_DE_DATOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Login()
+        {
+            SqlConnection conexion = new SqlConnection(@"server=CRISTIANRH\SQLEXPRESS; database=MiLogin; INTEGRATED SECURITY=true"
+);
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(" SELECT username, pass FROM registro WHERE username=@username AND pass=@pass",
+                conexion);
+            cmd.Parameters.AddWithValue("@username", txtusuario.Text.Trim());
+            cmd.Parameters.AddWithValue("@pass", txtpassword.Text.Trim());
+
+            SqlDataReader Lector = cmd.ExecuteReader();
+
+            if (Lector.Read())
+            {
+                conexion.Close();
+                MessageBox.Show("Login Exitoso");
+                Form1 Index = new Form1();
+                Index.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("You've entered incorrect login details", "ERROR",
+                MessageBoxButtons.OK);
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnregister_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+    }
+}
